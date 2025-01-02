6ebd9cd6790475821b7acb9377923aa4a554b765
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-01-02 14:57:59 +0300

    refactor: remove debug prints

--- GraphBLAS/FactoryKernels/GB_AxB__plus_times_fp32.c
+++ GraphBLAS/FactoryKernels/GB_AxB__plus_times_fp32.c
@@ -10,7 +10,6 @@
 #ifdef GBRISCV64
 #include <riscv_vector.h>
 #endif
-#include "stdio.h"
 #include "GB.h"
 #include "GB_control.h"
 #include "mxm/GB_AxB_saxpy.h"
@@ -303,7 +302,6 @@ GrB_Info GB (_Asaxpy4B__plus_times_fp32)
                 const int64_t *B_slice
             )
             {
-                printf("riscvhype!\n");
                 #include "mxm/template/GB_AxB_saxpy5_lv.c"
             }
 
