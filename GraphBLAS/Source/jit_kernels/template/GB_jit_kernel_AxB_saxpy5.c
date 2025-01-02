567932d494cb7b4e0a17397f8b92a7830aa97a74
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-01-02 15:24:46 +0300

    refactor: jit kernel rvv support

--- GraphBLAS/Source/jit_kernels/template/GB_jit_kernel_AxB_saxpy5.c
+++ GraphBLAS/Source/jit_kernels/template/GB_jit_kernel_AxB_saxpy5.c
@@ -8,6 +8,9 @@
 //------------------------------------------------------------------------------
 
 #include "include/GB_AxB_saxpy3_template.h"
+#ifdef GBRISCV64
+#include <riscv_vector.h>
+#endif
 
 GB_JIT_GLOBAL GB_JIT_KERNEL_AXB_SAXPY5_PROTO (GB_jit_kernel) ;
 
@@ -86,17 +89,6 @@ GB_JIT_GLOBAL GB_JIT_KERNEL_AXB_SAXPY5_PROTO (GB_jit_kernel) ;
         //----------------------------------------------------------------------
         // saxpy5 method with RISC-V vectors
         //----------------------------------------------------------------------
-        #define GB_V16_256 (16 * GB_Z_NBITS <= 256)
-        #define GB_V8_256  ( 8 * GB_Z_NBITS <= 256)
-        #define GB_V4_256  ( 4 * GB_Z_NBITS <= 256)
-
-        #undef  GB_V16
-        #undef  GB_V8
-        #undef  GB_V4
-
-        #define GB_V16 GB_V16_256
-        #define GB_V8  GB_V8_256
-        #define GB_V4  GB_V4_256
 
         #if GB_COMPILER_SUPPORTS_RVV1
 
@@ -110,7 +102,7 @@ GB_JIT_GLOBAL GB_JIT_KERNEL_AXB_SAXPY5_PROTO (GB_jit_kernel) ;
                 const int64_t *B_slice
             )
             {
-                #include "template/GB_AxB_saxpy5_unrolled.c"
+                #include "template/GB_AxB_saxpy5_lv.c"
             }
 
         #endif
